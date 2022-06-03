c5e522a1c1c90e8b3ba2a9924c0a0a44405ef5db
Abir Hussain Khan <37414078+abirhussain@users.noreply.github.com>
2022-06-04 01:51:06 +0600

    Create two_sum_II_input_array_is_sorted.py

---
+++ 167/two_sum_II_input_array_is_sorted.py
@@ -0,0 +1,12 @@
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        #two pointers
+        left_pointer = 0
+        right_pointer = len(numbers) - 1
+        while left_pointer < right_pointer:
+            tmp = numbers[left_pointer] + numbers[right_pointer]
+            if tmp == target:
+                return [left_pointer + 1, right_pointer+1]
+            elif tmp > target:
+                right_pointer -= 1
+            else:
+                left_pointer += 1
